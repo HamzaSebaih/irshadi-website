a073cd721e689ea915a5f4bdb9b82c217b513795
Senku150 <hamooz15@hotmail.com>
2025-01-31 22:29:57 +0300

    added a default colors in tailwindcss that we can change later

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD2JwoFCJuy9FDmALY4seFGIt5uTfM6EwM",
+  authDomain: "irshadi-auth.firebaseapp.com",
+  projectId: "irshadi-auth",
+  storageBucket: "irshadi-auth.firebasestorage.app",
+  messagingSenderId: "418916288228",
+  appId: "1:418916288228:web:8e34a3624a573293728ef1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#66D771',  // Lighter shade
+          DEFAULT: '#43CA52', // Main primary color
+          dark: '#329A3E'     // Darker shade
+        },
+        secondary: {
+          light: '#B0E6EB',
+          DEFAULT: '#96DCE2',
+          dark: '#70C2CA'
+        },
+        accent: {
+          light: '#86BCE0',
+          DEFAULT: '#64A8D4',
+          dark: '#4D8FBF'
+        }
+      }
+    }
   },
   plugins: [],
 }
