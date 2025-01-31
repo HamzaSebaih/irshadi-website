1d9d6f290b26c9e11df41c4e06753b78008af3a2
Senku150 <hamooz15@hotmail.com>
2025-01-31 21:49:53 +0300

    move the nav to the nav component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import StudentHomePage from './pages/StudnetPages/StudentHomePage';
+import Nav from "./components/Nav";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 const App = () => {
-
   const isAuth = true;
   const isStudent = true;
   return (
     <BrowserRouter>
-      <nav>
-        <ul>
-
-          {isAuth ? ( //here we first check if its logged in or not
-          //now we need other type of checker to check if is it a student or an admin
-          <>
-            {isStudent ? (
-              <li><Link to="/StudentHomePage">StudentHomePage</Link></li>
-            ): (
-              <li>this will be the link to admin home page and other nav staff</li>
-            )
-            }
-            
-          </>
-
-          ) : //if not logged in (isAuth) then show the login in nav bar
-          (
-            <>
-            <li><Link to="/">LoginPage</Link></li> 
-          </>
-
-          )}
-
-        </ul>
-      </nav>
+      <Nav isAuth={isAuth} isStudent={isStudent}/>
       <Routes>
         <Route path="/*" element={<LoginPage />} />
         <Route path="/StudentHomePage" element={<StudentHomePage />} />
