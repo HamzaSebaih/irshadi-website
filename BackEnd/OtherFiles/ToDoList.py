9644516d1ecf9015de41483319f1a4f0d896d97f
AbdulazizJastanieh <asalehjastanieh@stu.kau.edu.sa>
2025-03-24 12:21:56 +0300

    d
    d

--- BackEnd/OtherFiles/ToDoList.py
+++ BackEnd/OtherFiles/ToDoList.py
@@ -2,6 +2,8 @@
 1-change names of students and admins files based on UID from firebase authentication NOTE WIP
 2-creation of another collection named plans, which is just a list of courses. NOTE WIP
 
+3-keep in mind that the json file with our firestore credentials is not secure , it we make our github 
+#repo public , its GG
 NOW:
 
 
