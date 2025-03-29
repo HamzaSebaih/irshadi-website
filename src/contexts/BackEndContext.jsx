56f0dccaa32e2d9b6de63e919cbe2861b6c33528
hamza <hamooz15@hotmail.com>
2025-03-29 03:55:50 +0300

    fixed error that happnes sometimes when the user log in and logout and test it also I added some loading animiation every time the user fetch data from the backend

--- src/contexts/BackEndContext.jsx
+++ src/contexts/BackEndContext.jsx
@@ -50,7 +50,10 @@ export const ExtraInfoProvider = ({ children }) => {
 
   return (
     <ExtraInfoContext.Provider value={{ extraInfo, loadingExtra ,setUpdatedState}}>
-      {children}
+      {loadingExtra ?   <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    <p className="mt-4 text-lg font-semibold text-gray-700">Loading...</p>
+    </div> : children}
     </ExtraInfoContext.Provider>
   );
 };
