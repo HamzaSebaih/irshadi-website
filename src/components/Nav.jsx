3ef32875c1b74532e9658b7627af033158e68eb2
Senku150 <hamooz15@hotmail.com>
2025-04-12 05:00:44 +0300

    finished plan and courses section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,83 +2,106 @@ import { Link } from "react-router-dom";
 import { useAuth } from '../contexts/AuthContext';
 import LogoutButton from "./LogoutButton";
 import { useExtraInfo } from "../contexts/BackEndContext";
+import ShowCoursesPopUp from "./ShowCoursesPopUp";
+import { useState } from "react";
 
 const Nav = () => {
-  const { user } = useAuth(); //to check if user is logged in
-  const { extraInfo } = useExtraInfo(); //to check if the extra info is avaliable
-  const isAuth = user && extraInfo; //if both isn't null then the user is authratized to use the nav
-
-  const isAdmin = extraInfo?.role === "admin"
+  const { user } = useAuth(); // To check if user is logged in
+  const { extraInfo } = useExtraInfo(); // To check if extra info is available
+  const isAuth = user && extraInfo; // If both aren't null, the user is authorized
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const isAdmin = extraInfo?.role === "admin";
 
   return (
-    <nav className="bg-gray-800 p-4 shadow-md">
-      <div className="flex justify-between items-center">
+    <>
+      <nav className="bg-gray-800 p-4 shadow-md">
+        <div className="flex justify-between items-center">
+          {/* Left Side - Links */}
+          <ul className="flex space-x-6 text-white">
+            {isAuth ? (
+              <>
+                {isAdmin ? (
+                  <>
+                    <li className="text-gray-300">
+                      <Link
+                        to="/AdminHomePage"
+                        className="hover:text-gray-300 transition duration-300"
+                      >
+                        HomePage
+                      </Link>
+                    </li>
+                    <li className="text-gray-300">
+                      <Link
+                        to="/AdminStudyPlansPage"
+                        className="hover:text-gray-300 transition duration-300"
+                      >
+                        Study Plans
+                      </Link>
+                    </li>
+                    <li className="text-gray-300">
+                      <button
+                        className="hover:text-gray-300 transition duration-300"
+                        onClick={() => setIsPopupOpen(true)}
+                      >
+                        Courses
+                      </button>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                    <li>
+                      <Link
+                        to="/StudentHomePage"
+                        className="hover:text-gray-300 transition duration-300"
+                      >
+                        HomePage
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/AvailableForms"
+                        className="hover:text-gray-300 transition duration-300"
+                      >
+                        Available Forms
+                      </Link>
+                    </li>
+                  </>
+                )}
+              </>
+            ) : (
+              <li>
+                <Link
+                  to="/"
+                  className="hover:text-gray-300 transition duration-300"
+                >
+                  LoginPage
+                </Link>
+              </li>
+            )}
+          </ul>
 
-        {/* Left Side - Links */}
-        <ul className="flex space-x-6 text-white">
-          {isAuth ? (
-            <>
-              {isAdmin ? (
-                <>
-                <li className="text-gray-300">
-                                      <Link
-                      to="/AdminHomePage"
-                      className="hover:text-gray-300 transition duration-300"
-                    >
-                      HomePage
-                    </Link>
-                </li>
-                <li className="text-gray-300">
-                                      <Link
-                      to="/AdminStudyPlansPage"
-                      className="hover:text-gray-300 transition duration-300"
-                    >
-                      Study Plans
-                    </Link>
-                </li>
-
-                </>
-              ) : (
-                <>
-                  <li>
-                    <Link
-                      to="/StudentHomePage"
-                      className="hover:text-gray-300 transition duration-300"
-                    >
-                      HomePage
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/AvailableForms"
-                      className="hover:text-gray-300 transition duration-300"
-                    >
-                      Available Forms
-                    </Link>
-                  </li>
-                </>
-              )}
-            </>
-          ) : (
-            <li>
-              <Link
-                to="/"
-                className="hover:text-gray-300 transition duration-300"
-              >
-                LoginPage
-              </Link>
-            </li>
+          {/* Right Side - Logout Button */}
+          {isAuth && (
+            <div className="text-white">
+              <LogoutButton />
+            </div>
           )}
-        </ul>
-
-        {/* Right Side - Logout Button */}
-        {isAuth && (
-          <div className="text-white">
-            <LogoutButton />
+        </div>
+      </nav>
+      {(isPopupOpen && isAdmin) && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+          onClick={() => setIsPopupOpen(false)}
+        >
+          <div
+            className="bg-white p-4 rounded"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <ShowCoursesPopUp/>
           </div>
-        )}
-      </div>
-    </nav>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/pages/AdminPages/PlanDetailsPage.jsx
+++ src/pages/AdminPages/PlanDetailsPage.jsx
@@ -1,135 +1,191 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import ShowCoursesPopUp from '../../components/ShowCoursesPopUp';
 import { useEffect, useState } from 'react';
+import { useAuth } from '../../contexts/AuthContext';
 
 const PlanDetailsPage = () => {
+  const backendIp = "http://127.0.0.1:5000"; // Backend IP domain
   const navigate = useNavigate();
   const location = useLocation();
   const [isPopUpClicked, setIsPopUpClicked] = useState(false);
   const [parentCourse, setParentCourse] = useState(null);
   const [planID, setPlanID] = useState(null);
   const [planLevel, setPlanLevel] = useState(null);
-
-  const [planData, setPlanData] = useState({ //here when I'm done testing I will use fetch to fetch data from the backend TODO @Senku150
-    plan_id: location.state?.plan?.plan_id,
-    levels: {
-      "Level 1": [
-        { courseCode: "CPIT101", prerequisite: "None" },
-        { courseCode: "CPIT102", prerequisite: "CPIT101" },
-      ],
-      "Level 2": [
-        { courseCode: "CPIT201", prerequisite: "None" },
-        { courseCode: "CPIT202", prerequisite: "CPIT201" },
-      ],
-      Extra: [
-        { courseCode: "CPIT999", prerequisite: "None" },
-      ],
-    },
-  });
+  const dataFromLocation = location.state
+  const [planData, setPlanData] = useState(dataFromLocation?.plan);
+  const [isAddedNewCourse, setIsAddedNewCourse] = useState(false)
+  const [deleteCourseFromPlan, setDeleteCourseFromPlan] = useState(null);
+  const [deleteCourseFromPlanLevel, setDeleteCourseFromPlanLevel] = useState(null);
+  const [isLoading, setIsLoading] = useState(true)
+  const { user } = useAuth(); // Get current user from AuthContext
+  const token = user?.accessToken; // Access token for backend requests
 
   useEffect(() => {
     if (!location.state?.plan?.plan_id) {
       navigate("/AdminStudyPlansPage"); //this where fetching will happnes
     }
     else {
-      console.log("fetching WIP")
+      fetchCourses().finally(() => {
+        setIsAddedNewCourse(false)
+        setIsPopUpClicked(false)
+        setIsLoading(false)
+      });
     }
-  }, [location, navigate]);
+
+  }, [location, navigate, isAddedNewCourse, user]);
+
+  const fetchCourses = async () => {
+    try {
+      const response = await fetch(`${backendIp}/getPlans`, {
+        method: "GET",
+        headers: {
+          "Authorization": `${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      const matchingPlan = data.plans.find(plan => plan.plan_id === location.state?.plan?.plan_id);
+      if (matchingPlan) {
+        setPlanData(matchingPlan);
+      }
+
+    } catch (error) {
+      console.error("Error fetching extra info:", error);
+    }
+
+  };
+  useEffect(() => {
+    if (deleteCourseFromPlan && deleteCourseFromPlanLevel) {
+      setIsLoading(true)
+      sendDeleteCourse().finally(() => {
+        setIsLoading(false)
+        setDeleteCourseFromPlan(null);
+        setDeleteCourseFromPlanLevel(null);
+      });
+    }
+  }, [deleteCourseFromPlan, deleteCourseFromPlanLevel]);
+  const sendDeleteCourse = async () => {
+    let fixedPlanLevel = deleteCourseFromPlanLevel;
+    if (fixedPlanLevel.startsWith("Level")) {
+      fixedPlanLevel = parseInt(fixedPlanLevel.replace("Level", "").trim());
+    }
+
+    try {
+      const response = await fetch(`${backendIp}/deleteCourseFromPlan`, {
+        method: "POST",
+        headers: {
+          "Authorization": `${token}`,
+          "Content-Type": "application/json",
+        }, body: JSON.stringify({ plan_id: location.state?.plan?.plan_id, level_identifier: fixedPlanLevel, course_id: deleteCourseFromPlan }),
+      });
+      setIsAddedNewCourse(true)
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+    } catch (error) {
+      console.error("Error fetching extra info:", error);
+    }
+
+  };
 
   const handleClickConfirm = () => {
     navigate("/AdminStudyPlansPage");
   };
 
 
-
   const sortedLevelKeys = Object.keys(planData.levels);
 
   return (
-    <div className="container mx-auto p-4">
-      {/* Header */}
-      <div className="text-center my-4">
-        <h1 className="text-3xl font-bold">{planData.plan_id}</h1>
-      </div>
+    <>
+      {!isLoading ? (
+        <div className="container mx-auto p-4">
+          {/* Header */}
+          <div className="text-center my-4">
+            <h1 className="text-3xl font-bold">{planData.plan_id}</h1>
+          </div>
 
-      {/* Levels */}
-      <div className="space-y-8">
-        {sortedLevelKeys.map((levelKey) => (
-          <div key={levelKey} className="bg-gray-100 rounded-lg p-6 shadow">
-            <div className="mb-4">
-              <h2 className="text-2xl font-semibold">{levelKey}</h2>
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-              {planData.levels[levelKey].map((course, index) => (
-                <div key={index} className="bg-white p-4 border rounded shadow ">
-                  <span className="block text-lg font-medium mb-1">
-                    {course.courseCode}
-                  </span>
-                  <span className=" text-gray-600 block mb-3">
-                    Prerequisite: {course.prerequisite}
-                  </span>
+          {/* Levels */}
+          <div className="space-y-8">
+            {sortedLevelKeys.map((levelKey) => (
+              <div key={levelKey} className="bg-gray-100 rounded-lg p-6 shadow">
+                <div className="mb-4">
+                  <h2 className="text-3xl font-semibold">{levelKey}</h2>
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-4">
+                  {planData.levels[levelKey].map((course, index) => (
+                    <div key={index} className="bg-white p-6 border rounded shadow">
+                      <div className="bg-white p-4 border rounded shadow flex justify-between items-center">
+                        <span className="text-lg font-medium">{course}</span>
+                        <button
+                          onClick={() => {
+                            setDeleteCourseFromPlanLevel(levelKey)
+                            setDeleteCourseFromPlan(course);
+                          }}
+                          className="bg-red-500 hover:bg-red-600 text-white py-1 px-2 rounded"
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </div>
+                  ))}
                   <button
                     onClick={() => {
-                      setParentCourse(course)
-                      setPlanLevel(null)
-                      setPlanID(null)
-                      setIsPopUpClicked(true)
-                      
-                    }
-                    }
-                    className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+                      setPlanLevel(levelKey);
+                      setPlanID(location.state?.plan?.plan_id);
+                      setParentCourse(null);
+                      setIsPopUpClicked(true);
+                    }}
+                    className="flex items-center justify-center bg-white p-4 border rounded shadow hover:border-blue-300 hover:shadow-md transition"
                   >
-                    Select prerequisite
+                    <p className="w-20 h-20 flex items-center justify-center rounded-full bg-blue-500 text-white font-bold shadow">+</p>
                   </button>
                 </div>
-              ))}
-              <button
-                onClick={() => {
-                  setPlanLevel(levelKey)
-                  setPlanID(location.state?.plan?.plan_id)
-                  setParentCourse(null)
-                  setIsPopUpClicked(true)
-                  
-                }}
-                className="flex items-center justify-center bg-white p-4 border rounded shadow hover:border-blue-300 hover:shadow-md transition"
-              >
-                <p className="w-20 h-20 flex items-center justify-center rounded-full bg-blue-500 text-white font-bold shadow">+</p>
+              </div>
+            ))}
+          </div>
 
-              </button>
+          {/* Confirm Button */}
+          <div className="mt-6 text-center">
+            <button
+              onClick={handleClickConfirm}
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            >
+              Confirm
+            </button>
+          </div>
+
+          {isPopUpClicked && (
+            <div
+              className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+              onClick={() => setIsPopUpClicked(false)}
+            >
+              <div
+              onClick={(e) => e.stopPropagation()}>
+              <ShowCoursesPopUp
+                parentCourse={parentCourse}
+                planID={planID}
+                planLevel={planLevel}
+                setIsAddedNewCourse={setIsAddedNewCourse}
+                setIsLoadingForPage={setIsLoading}
+              />
             </div>
-          </div>
-        ))}
-      </div>
-
-      {/* Confirm Button */}
-      <div className="mt-6 text-center">
-        <button
-          onClick={handleClickConfirm}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-        >
-          Confirm
-        </button>
-      </div>
-      {isPopUpClicked && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
-          onClick={() => setIsPopUpClicked(false)} //if he enter outside it will exit
-        >
-          <div
-            className="bg-white p-6 rounded-lg shadow-lg w-96"
-            onClick={(e) => e.stopPropagation()} //to prevent the button from being render in the parent elemnts 
-          >
-            <ShowCoursesPopUp
-              parentCourse={parentCourse}
-              planID={planID}
-              planLevel={planLevel}
-            />
-
-          </div>
+            </div>
+  
+          )}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center h-screen">
+          <div className="w-16 h-16 border-4 border-t-transparent border-blue-500 border-solid rounded-full animate-spin"></div>
         </div>
       )}
-
-    </div>
+    </>
   );
-};
+}
 
 export default PlanDetailsPage;
