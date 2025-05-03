f1b7d02b75cb38b6669d5f623fd1293897b49194
AbdulazizJastanieh <asalehjastanieh@stu.kau.edu.sa>
2025-05-03 21:44:48 +0300

    removed files from github not necessary

--- BackEnd/OtherFiles/Testing.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-from firebase_admin import credentials, firestore, initialize_app
-
-# Initialize Firestore
-cred = credentials.Certificate("BackEnd/OtherFiles/irshadi-auth-firebase-adminsdk-fbsvc-9e96fac39e.json")
-initialize_app(cred)
-db = firestore.client()
