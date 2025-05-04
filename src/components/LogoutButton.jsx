afcfa11c73ac974f20b0b55a98e6ad55ba788116
Senku150 <hamooz15@hotmail.com>
2025-05-04 14:37:11 +0300

    edited the comments in logout button

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -16,7 +16,7 @@ function LogoutButton() {
         }
     };
   return (
-    // Styled similarly to inactive NavLink for consistency within the accent Nav
+
     <button
         className='rounded-md px-3 py-2 text-sm font-medium text-white transition-colors duration-150 hover:bg-accent-light hover:text-black focus:outline-none focus:ring-2 focus:ring-accent-light focus:ring-offset-2 focus:ring-offset-accent'
         onClick={handleLogout}
